add8687919b145121728ba29243413d0c7ad9bd1
Luis Guerrero <lmguerrerov@correo.udistrital.edu.co>
2022-05-10 14:12:55 -0500

    Calculadora 3

--- Calculadora3.py
+++
@@ -1,4 +0,0 @@
-print("esta es una prueba miguel")
-=======
-print("esta es una prueba 4")
->>>>>>> 1212f099d2eec0b70968655a10b5b79600357915
---
+++ Calduladora3.py
@@ -0,0 +1,3 @@
+# Este programa corresponde a una calculadora de conversion
+# Desarrollado para la clase de informatica.
+# Desarrollador: Luis Miguel Guerrero
